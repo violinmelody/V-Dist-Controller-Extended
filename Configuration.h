07cd1b479bca25ca1ee254ce00ab9b92b6147ce7
VIOLIN MELODY <35158589+violinmelody@users.noreply.github.com>
2026-09-13 21:04:26 +0200

    Update Configuration.h

--- Configuration.h
+++ Configuration.h
@@ -1,11 +1,21 @@
-/* Midi channel to use. */
-#define MIDI_CHANNEL 0
-
-/* Midi CC to use. */
+/* Output MIDI CC. */
 #define MIDI_CC 69
 
-/* Number of average measurements before data is used. */
+/* Incoming CC used to select a mode. Send this CC on MIDI channels 1-12.
+   The controller will then transmit MIDI_CC on that same channel. */
+#define MODE_SELECT_CC 119
+
+/* Number of ADC samples used for knob smoothing. */
 #define AVERAGE_COUNT 20
 
-/* Minimal change needed for controller to detect input. */
-#define MIN_CHANGE 0.5
+/* Minimum averaged ADC movement before accepting a new knob position. */
+#define MIN_CHANGE 0.5f
+
+/* CC update rate for continuously changing modes. */
+#define OUTPUT_INTERVAL_MS 10
+
+/* If MIDI Clock disappears for this long, clocked modulation freezes. */
+#define CLOCK_TIMEOUT_MS 750
+
+/* Default mode after power-up: 0 = MIDI channel 1 / Direct. */
+#define DEFAULT_MODE 0
